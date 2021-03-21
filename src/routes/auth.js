b8454ef3a8849fdaf6a7a121a21a86fde2d35cd1
mark-vilrokx <mark.vilrokx@hpe.com>
2021-03-21 00:26:32 -0700

    Remove uncalled middleware

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,18 +2,8 @@ const express = require("express");
 const { signup, login } = require("../middleware/authentication.js");
 const router = express.Router();
 
-router.post("/signup", signup, async (req, res, next) => {
-  res.json({
-    message: "Signup successful",
-    user: req.user,
-  });
-});
+router.post("/signup", signup);
 
-router.post("/login", login, async (req, res, next) => {
-  res.json({
-    message: "Signin successful",
-    user: req.user,
-  });
-});
+router.post("/login", login);
 
 module.exports = router;

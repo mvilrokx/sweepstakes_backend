071e8092b65ed55ca2a4cd6bbadd67865ee226e1
mark-vilrokx <mark.vilrokx@hpe.com>
2021-03-21 00:25:59 -0700

    refactor sanitization

--- src/lib/removeDisallowedFields.js
+++ src/lib/removeDisallowedFields.js
@@ -1,5 +1,9 @@
-module.exports = (obj, allowedKeys) =>
-  allowedKeys.reduce(
-    (reducedObj, key) => ({ ...reducedObj, [key]: obj[key] }),
-    {}
+module.exports = (...allowedKeys) => (...obj) => {
+  const sanitzedObj = obj.map(o =>
+    allowedKeys.reduce(
+      (reducedObj, key) => ({ ...reducedObj, [key]: o[key] }),
+      {}
+    )
   );
+  return sanitzedObj.length === 1 ? sanitzedObj[0] : sanitzedObj;
+};
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,31 +1,39 @@
 const bcrypt = require("bcrypt");
 
 const { db } = require("../lib/db.js");
-const removeDisallowedFields = require("../lib/removeDisallowedFields.js");
+const removeDisallowedFields = require("../lib/removeDisallowedFields.js")(
+  "email",
+  "id",
+  "super_admin"
+);
 const loadSql = require("../lib/loadSql.js");
-
-// TODO: Make this more "generic"
-const allowedFields = ["email", "id"];
-// TODO: Make this more "generic"
-// This should automatically apply to ALL response so that NO secret information EVER leaks to the client
 
 let sqls = {};
 
 (async () => (sqls = await loadSql(db, "../sql/user")))();
 
-const show = async id => await db.oneOrNone(sqls.showUser, { id });
-const index = async () => await db.any(sqls.indexUser);
+const show = async id =>
+  removeDisallowedFields(await db.one(sqls.showUser, { id }));
+// const user = await db.oneOrNone(sqls.showUser, { id });
+// return user
+//   ? removeDisallowedFields(await db.oneOrNone(sqls.showUser, { id }))
+//   : user;
+
+const index = async () =>
+  removeDisallowedFields(...(await db.any(sqls.indexUser)));
 
 const destroy = async id => await db.none(sqls.destroyUser, { id });
 
 const create = async ({ email, password, super_admin = false }) => {
   const hashedPwd = await bcrypt.hash(password, 10);
 
-  return await db.one(sqls.createUser, {
-    email,
-    password: hashedPwd,
-    super_admin,
-  });
+  return removeDisallowedFields(
+    await db.one(sqls.createUser, {
+      email,
+      password: hashedPwd,
+      super_admin,
+    })
+  );
 };
 
 const update = async (id, values) => {
@@ -37,7 +45,7 @@ const update = async (id, values) => {
   if (values.password) {
     updateValues.password = await bcrypt.hash(values.password, 10);
   }
-  return await db.one(sqls.updateUser, updateValues);
+  return removeDisallowedFields(await db.one(sqls.updateUser, updateValues));
 };
 
 const authenticate = async (email, password) => {
@@ -48,7 +56,7 @@ const authenticate = async (email, password) => {
 
   return !bcrypt.compareSync(password, user.password)
     ? null
-    : removeDisallowedFields(user, allowedFields);
+    : removeDisallowedFields(user);
 };
 
 module.exports = { show, index, destroy, create, update, authenticate };

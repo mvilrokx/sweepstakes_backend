660f2c49cf9d09d1a38b105b36d5f94b9b75f89d
mark-vilrokx <mark.vilrokx@hpe.com>
2021-04-03 14:43:02 -0700

    Improve error handling of loading SQL files

--- src/lib/loadSql.js
+++ src/lib/loadSql.js
@@ -1,21 +1,26 @@
 const { stat, readdir } = require("fs/promises");
 const { join, parse } = require("path");
+const Logger = require("./logger");
 
 const sqls = {};
 
 async function loadSql(db, ...sqlFiles) {
   for (const sqlFile of sqlFiles) {
-    const stats = await stat(join(__dirname, sqlFile));
-    if (stats.isFile()) {
-      sqls[parse(sqlFile).name] = new db.$config.pgp.QueryFile(
-        join(__dirname, sqlFile),
-        { minify: true }
-      );
-    } else if (stats.isDirectory()) {
-      const files = (await readdir(join(__dirname, sqlFile))).map(fName =>
-        join(sqlFile, fName)
-      );
-      await loadSql(db, ...files);
+    try {
+      const stats = await stat(join(__dirname, sqlFile));
+      if (stats.isFile()) {
+        sqls[parse(sqlFile).name] = new db.$config.pgp.QueryFile(
+          join(__dirname, sqlFile),
+          { minify: true }
+        );
+      } else if (stats.isDirectory()) {
+        const files = (await readdir(join(__dirname, sqlFile))).map(fName =>
+          join(sqlFile, fName)
+        );
+        await loadSql(db, ...files);
+      }
+    } catch (err) {
+      Logger.error(`Unable to load SQL File or Directory ${sqlFile}`);
     }
   }
   return sqls;
